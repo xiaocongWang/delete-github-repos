d46c7b59ac6ecd9e021ff319db74f85802f56393
wangxiaocong <wangxiaocong@didiglobal.com>
2021-08-16 15:21:36 +0800

    使用 token 验证用户

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 const commander = require('commander');
 const colors = require('colors');
 const GithubTool = require('../lib/githubTool');
-const { deleteReposPrompt, checkboxReposPrompt } = require('../lib/prompt');
+const { usernamePrompt, tokenPrompt, deleteReposPrompt, checkboxReposPrompt } = require('../lib/prompt');
 const pkg = require('../package.json');
 
 function separatedList(value) {
@@ -15,22 +15,60 @@ function separatedList(value) {
 
 commander
     .version(pkg.version, '-v, --version')
-    .option('-u, --user <user:password>', 'USER:PASSWORD Github user and password')
     .option('-r, --repos <reposNames>', 'Repos need to delete, perhaps String or String of Array', separatedList)
 
 commander.parse(process.argv);
 
-let { user, repos } = commander;
-const userInfo = user && user.split(':');
+const { repos } = commander;
 
-if (!user || userInfo.length !== 2) {
-    console.error(`delete-github-repos: try 'delete-github-repos --help' for more information`);
-    process.exit(1);
+let githubTool;
+
+init();
+
+async function init() {
+    const username = await usernamePrompt();
+
+    let token = await tokenPrompt();
+
+    githubTool = new GithubTool(username, token);
+
+    // token 校验
+    console.log(colors.yellow('\ntoken 验证中...\n'));
+
+    const isTrue = await githubTool.checkUser(username);
+
+    // 如果校验不通过则直接退出
+    if (!isTrue) {
+        console.log(colors.red('token 校验失败\n'));
+        process.exit(1);
+    }
+
+    if (isTrue) {
+        // 如果 repos 不存在，则去拉取所有仓库，并且让用户选择删除哪些
+        if (!repos) {
+            // 开始加载所有仓库
+            console.log(colors.yellow('\n验证成功，加载所有仓库中...\n'));
+            const repos = await getOwnerRepos();
+
+            if (repos.length === 0) {
+                console.log(colors.green('您没有自己的拥有仓库\n'));
+                process.exit(1);
+            } else {
+                const choices = repos.map(item => ({ name: item }))
+                const answers = await checkboxReposPrompt(choices);
+                if (answers.length > 0) {
+                    const isDelete = await deleteReposPrompt(answers);
+                    isDelete && deleteRepos(answers);
+                } else {
+                    process.exit(1);
+                }
+            }
+        } else if (repos && repos.length > 0) {
+            const isDelete = await deleteReposPrompt(repos);
+            isDelete && deleteRepos(repos);
+        }
+    }
 }
-
-const [ username, password ] = userInfo;
-
-const githubTool = new GithubTool(username, password);
 
 async function getOwnerRepos() {
     try {
@@ -51,57 +89,20 @@ async function getOwnerRepos() {
     }
 }
 
-async function deleteRepos(answer, repos) {
-    if (answer) {
-        // 开始校验账号密码是否正确
-        console.log(colors.yellow('\n密码验证中...\n'));
-        if (await githubTool.checkUser()) {
-            console.log(colors.green(`密码校验通过，开始删除 ${repos.join('，')} 仓库\n`));
+async function deleteRepos(repos) {
+    console.log(colors.green(`开始删除 ${repos.join('，')} 仓库\n`));
+    // 开始删除仓库
+    try {
+        // 部分删除失败的
+        const fails = await githubTool.deleteRepos(repos);
+
+        if (fails.length > 0) {
+            console.log(colors.red(`${fails.join('、')} 仓库删除失败\n` ));
         } else {
-            console.log(colors.red('密码校验不通过，输入正确的账号和密码\n'));
-            process.exit(1);
-        };
-
-        // 开始删除仓库
-        try {
-            // 部分删除失败的
-            const fails = await githubTool.deleteRepos(repos);
-
-            if (fails.length > 0) {
-                console.log(colors.red(`${fails.join('、')} 仓库删除失败\n` ));
-            } else {
-                console.log(colors.green('成功删除所有仓库\n'));
-            }
-        } catch(err) {
-            console.log(colors.red(err.toString()));
-            process.exit(1);
+            console.log(colors.green('成功删除所有仓库\n'));
         }
+    } catch(err) {
+        console.log(colors.red(err.toString()));
+        process.exit(1);
     }
-}
-
-// 如果 repos 不存在，则去拉取所有仓库，并且让用户选择删除哪些
-if (!repos) {
-    // 开始加载所有仓库
-    console.log(colors.yellow('\n加载所有仓库中...\n'));
-    getOwnerRepos().then(repos => {
-        if (repos.length === 0) {
-            console.log(colors.green('您没有自己的拥有仓库\n'));
-            process.exit(1);
-        } else {
-            const choices = repos.map(item => ({ name: item }))
-            checkboxReposPrompt(choices, (answers) => {
-                if (answers.length > 0) {
-                    deleteReposPrompt(answers, (isDelete) => {
-                        deleteRepos(isDelete, answers);
-                    });
-                } else {
-                    process.exit(1);
-                }
-            });
-        }
-    });
-} else if (repos && repos.length > 0) {
-    deleteReposPrompt(repos, (answer) => {
-        deleteRepos(answer, repos);
-    });
 }
